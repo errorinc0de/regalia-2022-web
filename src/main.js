4a22d14c08a500f04ea00a7db6f59d20ba048328
asweetrain <rishav.m.19.02@gmail.com>
2022-05-07 13:46:35 +0530

    complete homepage ui

--- src/main.js
+++ src/main.js
@@ -3,11 +3,22 @@ import './tailwind.css'
 import { el, elall, log } from './util.js'
 
 const nav = el('nav[data-navbar]')
+const navlinks = elall('a[data-navlink]')
 
-window.onscroll = e => {
-  const scrollY = window.pageYOffset
+const sections = elall('section')
+const section_home = el('section[data-home]')
+const section_event = el('section[data-event]')
+const section_artist = el('section[data-artist]')
+const section_about = el('section[data-about]')
+const section_contact = el('section[data-contact]')
 
-  scrollY > 10
+onscroll = () => {
+// add background on scroll
+  pageYOffset > 10
     ? nav.classList.add('bg-background')
     : nav.classList.remove('bg-background')
+
+  //TODO add active to current link
+
 }
+
--- src/util.js
+++ src/util.js
@@ -1,5 +1,5 @@
 // console log
-export const log = console.log
+export const log = console.debug
 // query selector
 export const el = (selector, type = document) => type.querySelector(selector)
 // query selector all
