3cdc27847ea56a8e2b67972061673daaa9d427be
fallin-rain <102976091+fallin-rain@users.noreply.github.com>
2022-05-09 19:17:13 +0530

    Update vite.config.js
    this needed to be added else nested html files won't bundle together when building for production.😌🚀

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
-module.exports = {
-  root: 'src',
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+	base: '',
+	root: 'src',
   build: {
-    outDir: '../dist',
-  },
-}
+		outDir: '../deploy',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src', 'index.html'),
+        about: resolve(__dirname, 'src', 'about/index.html'),
+        events: resolve(__dirname, 'src', 'events/index.html')
+      }
+    }
+  }
+})
